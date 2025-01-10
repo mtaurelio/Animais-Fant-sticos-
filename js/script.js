a39a9b46c641129152c352bfdc69021cfc298ceb
Matheus Aurelio <matheusaureliodeo4@gmail.com>
2025-01-10 20:02:29 -0300

    tab-nav refatorado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
+import TabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdown from "./modules/dropdown.js";
@@ -15,11 +15,16 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-initAnimacaoScroll();
-initTabNav();
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
+
 initModal();
 initTooltip();
 initDropdown();
 initMenumobile();
 initAnimaNumeros();
 initPraticas();
+initAnimacaoScroll();
